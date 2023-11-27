800c2694bf66f3f912a1e2405cc2b69f07c971de
matheusadami.lima@gmail.com <matheusadami.lima@gmail.com>
2023-11-27 16:03:27 -0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Random from "./components/Random";
 import Card from "./components/layout/Card";
+import Familia from "./components/basicos/Familia";
+import FamiliaMembro from "./components/basicos/FamiliaMembro";
 
 
 export default () =>
@@ -19,6 +21,13 @@ export default () =>
                 <Random 
                 min={1} max={10}
                 /> 
+            </Card>
+            <Card titulo="Componente com Filhos" color="#6c178b">
+             <Familia sobrenome="Lima">
+                <FamiliaMembro nome="Pedro" />
+                <FamiliaMembro nome="Ana"  />
+                <FamiliaMembro nome="Gustavo" />
+             </Familia>
             </Card>
         </div>
     
